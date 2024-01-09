9700af7efbace17f0c8b0d427ca5d958f58e4695
Tuyet Nguyen <127586258+TuyetNNguyen@users.noreply.github.com>
2024-01-09 12:13:11 -0800

    Update CSVCovidReader.java

--- datamanagement/CSVCovidReader.java
+++ datamanagement/CSVCovidReader.java
@@ -1,6 +1,6 @@
-package edu.upenn.cit594.datamanagement;
-import edu.upenn.cit594.util.CovidObject;
-import edu.upenn.cit594.util.MemoryStorage;
+package filereader.datamanagement;
+import filereader.util.CovidObject;
+import filereader.util.MemoryStorage;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
